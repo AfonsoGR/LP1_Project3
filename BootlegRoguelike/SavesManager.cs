3f44eb7666933df1503446fdffe84d5973bea4a1
AfonsoGR <afonso_rosa@hotmail.com>
2020-06-09 17:57:54 +0100

    Add way to register highscores WIP

--- BootlegRoguelike/SavesManager.cs
+++ BootlegRoguelike/SavesManager.cs
@@ -1,3 +1,7 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
 namespace BootlegRoguelike
 {
     public class SavesManager
@@ -9,6 +13,21 @@ namespace BootlegRoguelike
         {}
 
         private void RegisterHighscore()
-        {}
+        {
+            const string fileScores = "highscores.txt";
+            Queue <string> queueScores = new Queue<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                queueScores.Enqueue(input);
+            }
+
+            File.WriteAllLines(fileScores, queueScores);
+        }
     }
 }
